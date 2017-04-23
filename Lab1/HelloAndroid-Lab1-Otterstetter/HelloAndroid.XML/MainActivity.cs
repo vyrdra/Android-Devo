ec2e931c73251052de978ced1bc5ba36b5313d17
Aaron Otterstetter <ottersteterai@hotmail.com>
2017-04-22 23:00:38 -0700

    lab1

---
+++ Lab1/HelloAndroid-Lab1-Otterstetter/HelloAndroid.XML/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid.XML
+{
+    [Activity(Label = "HelloAndroid.XML", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            var aButton = FindViewById<Button>(Resource.Id.aButton);
+            var aLabel = FindViewById<TextView>(Resource.Id.aLabel);
+            var aLabel2 = FindViewById<TextView>(Resource.Id.aLabel2);
+            var clearButton = FindViewById<Button>(Resource.Id.clearButton);
+
+            aButton.Click += (sender, e) => 
+                { aLabel2.Text = "Hello from the button"; };
+            clearButton.Click += (sender, e) => { aLabel.Text = ""; aLabel2.Text = ""; };
+
+        }
+    }
+}
+
---
+++ Lab1/HelloAndroid-Lab1-Otterstetter/HelloAndroid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //create the user interface in code
+            LinearLayout layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+
+            TextView aLabel = new TextView(this);
+            TextView Label2 = new TextView(this);
+            aLabel.Text = "Hello, Xamarin Android";
+            Label2.Text = "---";
+
+            Button aButton = new Button(this);
+            Button Button2 = new Button(this);
+            aButton.Text = "Say Hello";
+            Button2.Text = "Clear";
+            aButton.Click += (sender, e) => 
+                { Label2.Text = "Hello from the button again"; };
+            Button2.Click += (sender, e) => { aLabel.Text = ""; Label2.Text = ""; };
+
+            layout.AddView(aLabel);
+            layout.AddView(Label2);
+            layout.AddView(aButton);
+            layout.AddView(Button2);
+            SetContentView(layout);
+        }
+    }
+}
+
